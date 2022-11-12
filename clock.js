0ca63658a479a887867f24ca2a04faa4b5003a66
cnhayes24 <93173774+cnhayes24@users.noreply.github.com>
2022-11-12 10:32:31 -0600

    Update clock.js

--- clock.js
+++ clock.js
@@ -28,66 +28,64 @@ var sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/animals/duck
             secs = secs < 10 ? "0" + secs : secs;
           
            //display current time 
-            let currentTime = hours + ":"
-                + mins + ":" + secs + " " + am_pm;
-            document.getElementById("clock")
-                .innerHTML = currentTime;
+           let currentTime = hours + ":"
+               + mins + ":" + secs + " " + am_pm;
+           document.getElementById("clock")
+               .innerHTML = currentTime;
          
-            //gets the current date
-            var currentDate  = new Date(),
-            currentDay   = currentDate.getDate() < 10 
-                 ? '0' + currentDate.getDate() 
-                 : currentDate.getDate(),
-            currentMonth = currentDate.getMonth() < 9 
-                 ? '0' + (currentDate.getMonth() + 1) 
-                 : (currentDate.getMonth() + 1);
+           //gets the current date
+           var currentDate  = new Date(),
+           currentDay   = currentDate.getDate() < 10 
+                ? '0' + currentDate.getDate() 
+                : currentDate.getDate(),
+           currentMonth = currentDate.getMonth() < 9 
+                ? '0' + (currentDate.getMonth() + 1) 
+                : (currentDate.getMonth() + 1);
          
-            //date formatting
-            let dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            let months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
+           //date formatting
+           let dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+           let months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
              
-            //displays date
-            document.getElementById("date").innerHTML = months[currentMonth - 1] + " " + currentDay + ", " + currentDate.getFullYear();
+           //displays date
+           document.getElementById("date").innerHTML = months[currentMonth - 1] + " " + currentDay + ", " + currentDate.getFullYear();
           
      
-  }
-  showTime();
+     }
+     showTime();
+      
+     //function to update time if less than 10
+     function addZero(time) {
+	     return (time < 10) ? "0" + time : time;
+     }
 
-function addZero(time) {
-
-		return (time < 10) ? "0" + time : time;
+     //function to set alarm time
+     function setAlarm() {
+	     var hr = document.getElementById('alarmHrs');
 	
-}
-
-//function to set alarm time
-function setAlarm() {
-
-	var hr = document.getElementById('alarmHrs');
+	     var min = document.getElementById('alarmMins');
+  
+             var secs = document.getElementById('alarmSecs');
 	
-	var min = document.getElementById('alarmMins');
+	     var ap = document.getElementById('ampm');
+	     
+	     //variables get value of selected alarmtime
+	     var selectedHour = hr.options[hr.selectedIndex].value;
+             var selectedMin = min.options[min.selectedIndex].value;
+             var selectedSec = secs.options[secs.selectedIndex].value;
+             var selectedAP = ap.options[ap.selectedIndex].value;
   
-  var secs = document.getElementById('alarmSecs');
-	
-	var ap = document.getElementById('ampm');
+     //disables alarm dropdown menus once alarm set
+     document.getElementById('alarmHrs').disabled = true;
+     document.getElementById('alarmMins').disabled = true;
+     document.getElementById('alarmSecs').disabled = true;
+     document.getElementById('ampm').disabled = true;
+	     
+     //alarm time formatting
+     var alarmTime = selectedHour + ":" + selectedMin + ":" + selectedSec + selectedAP;
   
-  var selectedHour = hr.options[hr.selectedIndex].value;
-  var selectedMin = min.options[min.selectedIndex].value;
-  var selectedSec = secs.options[secs.selectedIndex].value;
-  var selectedAP = ap.options[ap.selectedIndex].value;
-  
-  
-  //var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) +  selectedAP;
-  
-  document.getElementById('alarmHrs').disabled = true;
-	document.getElementById('alarmMins').disabled = true;
-  document.getElementById('alarmSecs').disabled = true;
-	document.getElementById('ampm').disabled = true;
-  
- var alarmTime = selectedHour + ":" + selectedMin + ":" + selectedSec + selectedAP;
-  
-
-setInterval(function(){
-            let time = new Date();
+     //function to compare alarm and current time
+     setInterval(function(){
+	    let time = new Date();
             let hours = time.getHours();
             let mins = time.getMinutes();
             let secs = time.getSeconds();
@@ -108,29 +106,27 @@ setInterval(function(){
             mins = mins < 10 ? "0" + mins : mins;
             secs = secs < 10 ? "0" + secs : secs;
           
-   //gets current time 
-     var currentTime = hours + ":" + mins + ":" + secs + am_pm;
+            //gets current time 
+            var currentTime = hours + ":" + mins + ":" + secs + am_pm;
   
-   //gets alarm time
-   var alarmTime = selectedHour + ":" + selectedMin + ":" + selectedSec + selectedAP;
-    //var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + selectedAP;
-  
-   //sounds the set alarm
-  if (alarmTime == currentTime){
-    //console.log('time');
+           //gets alarm time
+           var alarmTime = selectedHour + ":" + selectedMin + ":" + selectedSec + selectedAP;
+        
+           //sounds the set alarm
+          if (alarmTime == currentTime){
+                //console.log('time');
 		sound.play();
-	}
-},1000);
+	  }
+     },1000);
 
-}
+     }
 
-//function to stop alarm
-function clearAlarm() {
-
+    //function to stop alarm
+    function clearAlarm() {
 	document.getElementById('alarmHrs').disabled = false;
 	document.getElementById('alarmMins').disabled = false;
-  document.getElementById('alarmSecs').disabled = false;
+        document.getElementById('alarmSecs').disabled = false;
 	document.getElementById('ampm').disabled = false;
 	sound.pause();
-  sound.currentTime = 0;
-}
+        sound.currentTime = 0;
+   }
